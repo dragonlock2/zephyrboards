3ac357fca4e5e4690e2c33af64b980a9f4ea7471
Matthew Tran <matthewlamtran@berkeley.edu>
2022-01-16 06:42:31 -0800

    added syscon driver

--- drivers/clock_control/clock_control_lpc84x.c
+++ drivers/clock_control/clock_control_lpc84x.c
@@ -1,3 +1,50 @@
 #define DT_DRV_COMPAT nxp_lpc84x_syscon
 
+#include <devicetree.h>
+#include <device.h>
 
+#include <drivers/clock_control.h>
+#include <fsl_clock.h>
+
+#define LOG_LEVEL CONFIG_CLOCK_CONTROL_LOG_LEVEL
+#include <logging/log.h>
+LOG_MODULE_REGISTER(clock_control);
+
+static int lpc84x_syscon_on(const struct device *dev,
+                            clock_control_subsys_t sub_system) {
+    clock_ip_name_t clock_ip_name = (clock_ip_name_t) sub_system;
+    CLOCK_EnableClock(clock_ip_name);
+    return 0;
+}
+
+static int lpc84x_syscon_off(const struct device *dev,
+                             clock_control_subsys_t sub_system) {
+    clock_ip_name_t clock_ip_name = (clock_ip_name_t) sub_system;
+    CLOCK_DisableClock(clock_ip_name);
+    return 0;
+}
+
+static int lpc84x_syscon_get_subsys_rate(const struct device *dev,
+                                         clock_control_subsys_t sub_system,
+                                         uint32_t *rate) {
+    clock_name_t clock_name = (clock_name_t) sub_system;
+    *rate = CLOCK_GetFreq(clock_name);
+    return 0;
+}
+
+static int lpc84x_syscon_init(const struct device *dev) {
+    return 0;
+}
+
+static const struct clock_control_driver_api lpc84x_syscon_driver_api = {
+    .on = lpc84x_syscon_on,
+    .off = lpc84x_syscon_off,
+    .get_rate = lpc84x_syscon_get_subsys_rate,
+};
+
+DEVICE_DT_INST_DEFINE(0,
+                      &lpc84x_syscon_init,
+                      NULL,
+                      NULL, NULL,
+                      PRE_KERNEL_1, CONFIG_CLOCK_CONTROL_INIT_PRIORITY,
+                      &lpc84x_syscon_driver_api);
--- samples/lpc845brk_test/src/main.c
+++ samples/lpc845brk_test/src/main.c
@@ -1,6 +1,15 @@
 #include <zephyr.h>
 
+#include <device.h>
+#include <drivers/clock_control.h>
+#include <fsl_clock.h>
+
+const struct device* dev = DEVICE_DT_GET(DT_NODELABEL(syscon));
+
 int main() {
+
+    clock_control_on(dev, (clock_control_subsys_t) kCLOCK_Swm);
+
     volatile int8_t c = 0;
     while (1) {
         printk("hi! %d\r\n", c++);
