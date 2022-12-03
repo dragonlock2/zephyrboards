9c38f692abc22912042c8d1835eb5738b9d99edd
Matthew Tran <matthewlamtran@berkeley.edu>
2022-12-03 03:45:33 -0800

    added lazy LIN motor interface

--- samples/stm32_esc/src/main.c
+++ samples/stm32_esc/src/main.c
@@ -9,23 +9,47 @@ LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
 const struct device *lin = DEVICE_DT_GET_ANY(zephyrboards_lin_uart);
 
-int main() {
-    lin_set_mode(lin, LIN_MODE_RESPONDER);
-    lin_set_bitrate(lin, 19200);
+LIN_MSGQ_DEFINE(reqs, 10);
 
+void tx_cb(const struct device *dev, int error, void *user_data) {}
+
+int main() {
     rgb_init();
     sense_init();
     motor_init();
 
+    lin_set_mode(lin, LIN_MODE_RESPONDER);
+    lin_set_bitrate(lin, 19200);
+
+    struct zlin_filter rx_filt = {
+        .checksum_type = LIN_CHECKSUM_ENHANCED,
+        .data_len = 2,
+        .id = 0x00,
+    };
+    lin_add_rx_filter_msgq(lin, &reqs, &rx_filt);
+
     LOG_DBG("booted!");
+    motor_write(0.1, true); // default
     rgb_color_E c = 0;
     while (1) {
-        motor_write(0.1, true);
-        LOG_DBG("cycle time us: %d", motor_cycle_time_us());
+        // should be handling in interrupt, just lazy :P
+        struct zlin_frame msg;
+        if (k_msgq_get(&reqs, &msg, K_NO_WAIT) == 0) {
+            motor_write(msg.data[0] / 255.0, msg.data[1] & 0x01);
+        }
+
+        uint32_t t = motor_cycle_time_us();
+        msg.id = 0x01;
+        msg.checksum_type = LIN_CHECKSUM_ENHANCED;
+        msg.data_len = 4;
+        msg.data[0] = (t >> 24) & 0xFF; // MSB first
+        msg.data[1] = (t >> 16) & 0xFF;
+        msg.data[2] = (t >> 8)  & 0xFF;
+        msg.data[3] = (t >> 0)  & 0xFF;
+        lin_send(lin, &msg, K_NO_WAIT, tx_cb, NULL);
 
         rgb_write(c);
         c = c == RGB_COLOR_COUNT - 1 ? 0 : c + 1;
-        k_msleep(500);
-        // TODO feedback over LIN
+        k_msleep(50);
     }
 }
