be3065fc11d7f6ec995bc65da85ef6d709ea1b21
Matthew Tran <matthewlamtran@berkeley.edu>
2024-08-21 22:52:46 -0700

    minor cleaning

--- samples/apps/rubberducky/src/bos_desc.h
+++ samples/apps/rubberducky/src/bos_desc.h
@@ -5,7 +5,7 @@
 #include <zephyr/usb/usb_ch9.h>
 
 #ifdef CONFIG_USB_DEVICE_BOS
-#error "os detection workaround not compatabile with CONFIG_USB_DEVICE_BOS"
+#error "os detection workaround not compatible with CONFIG_USB_DEVICE_BOS"
 #endif
 
 int usb_handle_bos(struct usb_setup_packet *setup, int32_t *len, uint8_t **data);
